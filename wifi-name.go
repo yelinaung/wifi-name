c7a0151f0b14767c9b7c5d1d0e6bb8ddbd1ab692
Ye Lin Aung <me@yelinaung.com>
2016-01-26 16:07:29 +0630

    Added linux support but not tested yet

--- wifi-name.go
+++ wifi-name.go
@@ -5,9 +5,13 @@ import (
 	"os/exec"
 	"regexp"
 	"runtime"
+	"strings"
 )
 
 const osxCmd = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
+const osxArgs = "-I"
+const linuxCmd = "iwgetid"
+const linuxArgs = "--raw"
 
 func WifiName() string {
 	platform := runtime.GOOS
@@ -18,17 +22,36 @@ func WifiName() string {
 		return ""
 	} else {
 		// TODO for Linux
-		return ""
+		return forLinux()
 	}
 }
 
-func forOSX() string {
-	cmd := exec.Command(osxCmd, "-I")
-
+func forLinux() string {
+	cmd := exec.Command(linuxCmd, linuxArgs)
 	stdout, err := cmd.StdoutPipe()
-	if err != nil {
+	panicIf(err)
+
+	// start the command after having set up the pipe
+	if err := cmd.Start(); err != nil {
 		panic(err)
 	}
+
+	var str string
+
+	if b, err := ioutil.ReadAll(stdout); err == nil {
+		str += (string(b) + "\n")
+	}
+
+	name := strings.Replace(str, "\n", "", -1)
+	return name
+}
+
+func forOSX() string {
+
+	cmd := exec.Command(osxCmd, osxArgs)
+
+	stdout, err := cmd.StdoutPipe()
+	panicIf(err)
 
 	// start the command after having set up the pipe
 	if err := cmd.Start(); err != nil {
@@ -49,5 +72,11 @@ func forOSX() string {
 		return "Could not get SSID"
 	} else {
 		return name[1][1]
+	}
+}
+
+func panicIf(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
